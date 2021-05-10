315dfae7f954566b41c14f78a82238aa08b4d0d9
Martin McWhorter <martin.mcwhorter@verizonconnect.com>
2021-05-10 18:17:38 +0100

    chore: updated to 9

--- demo/src/app/app.module.ts
+++ demo/src/app/app.module.ts
@@ -4,7 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AddressFormComponent } from './address-form/address-form.component';
-import { MatInputModule, MatButtonModule, MatSelectModule, MatRadioModule, MatCardModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ErrorComponent } from './error/error.component';
 
